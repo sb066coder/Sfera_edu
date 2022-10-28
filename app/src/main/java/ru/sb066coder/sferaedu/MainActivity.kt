7eb6efdbb93e32b0204cafed9f8f36fafaeb5eb7
Sergey Buksha <sergey066@yandex.ru>
2022-10-28 17:41:05 +0300

    Initial commit

---
+++ app/src/main/java/ru/sb066coder/sferaedu/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.sb066coder.sferaedu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
